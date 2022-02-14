83facc57e2034577ad0e4c09d5542338a58b3ff4
inkdebut <37120802+inkdebut@users.noreply.github.com>
2022-02-15 01:04:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,46 @@
 # -*- coding: utf-8 -*-
-# @Author  : 扪参历井仰胁息の蔡裕成
+# @Author : Cai Yucheng
 # @FileName: main.py
-# @Date    : 2022/1/13 一月 2022
-# @Time    : 11:21
+# @Date : 1/13/2022 January 2022
+# @Time : 11:21
 # @Project : hand_movement_trajectory
 
-#Warning:
-#These comments were all written in Chinese. due to time constraints and some reasons, 
-#they can not be changed one by one. You can choose to copy them and then go to relevant websites or software for translation.
+
+#based on cv2 and mediapipe (medium media), detect the hand in the picture captured by the camera, and draw the joint connection and track its movement trajectory
+#if you want to modify the accuracy and running speed of the detection and the number of hands that the camera can detect, you can modify the parameters in Hand()
 
 
-#基于cv2和mediapipe(介质媒体),检测摄像头所拍到的画面中的手，并画出关节连线与追踪其运动轨迹
-#想修改检测的精度与运行速度以及摄像头所能检测到的手的数量可以在Hand()中修改参数
-
-
-#导入包
+#import package
 import time
 import cv2
 import mediapipe as mp
 
-#捕捉摄像头，如果只有一个则默认为0
+#capture the camera, if there is only one, the default is 0
 cap = cv2.VideoCapture(0)
 
-#调用手部函数
+#call hand function
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 
-#画手部的线条
+#draw the lines of the hands
 mpDraw = mp.solutions.drawing_utils
 
-handlmsStyle=mpDraw.DrawingSpec(color=(255,255,255),thickness=5) #手部的关节点，颜色为白色，粗细设置为5
-handconStyle=mpDraw.DrawingSpec(color=(0,255,0),thickness=3) #手部的线条，颜色为绿色，粗细设置为3
+handlmsStyle=mpDraw.DrawingSpec(color=(255,255,255),thickness=5) #the joint point of the hand, the color is white, and the thickness is set to 5
+handconStyle=mpDraw.DrawingSpec(color=(0,255,0),thickness=3) #the line of the hand, the color is green, and the thickness is set to 3
 
-#帧率显示所需要的变量
-pTime = 0    #present time  循环结尾设置pTime=cTime，然后在下一次循环中得出循环中的cTime-pTime，fps=1/cTime-pTime，即当前帧率
-cTime = 0    #current time 当前时间，即每次循环所增加的时间
+#frame rate display required variables
+pTime = 0 #present time Set pTime=cTime at the end of the loop, and then get the cTime-pTime in the loop in the next loop, fps=1/cTime-pTime, which is the current frame rate
+cTime = 0 #current time The current time, that is, the time added by each loop
 
-#主循环
+#main loop
 while True:
     ret, img = cap.read()
     imgWidth = img.shape[1]
     imgHeight = img.shape[0]
     if ret:
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #将颜色由BGR转为RGB
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #convert the color from BGR to RGB
         result = hands.process(imgRGB)
-        print(result.multi_hand_landmarks)  #手部的坐标
+        print(result.multi_hand_landmarks) #the coordinates of the hand
         if result.multi_hand_landmarks:
             for handlms in result.multi_hand_landmarks:
                 mpDraw.draw_landmarks(img,handlms,mpHands.HAND_CONNECTIONS,handlmsStyle,handconStyle)
@@ -52,27 +48,25 @@ while True:
 
                     xPos = int(lm.x*imgWidth)
                     yPos = int(lm.y*imgHeight)
-                    #打印手部关节点的数字，数字大小为0.3，颜色为红色
+                    #print the number of hand joint points, the number size is 0.3, and the color is red
                     cv2.putText(img,str(i),(xPos-25,yPos+5),cv2.FONT_HERSHEY_SIMPLEX,0.3,(0,0,255),2)
-                    #当i为4，即大拇指，点会放大，大小设定为10
+                    #when i is 4, that is, the thumb, the point will be enlarged, and the size is set to 10
                     if i==4:
                         cv2.circle(img,(xPos,yPos),10,(0,0,255),cv2.FILLED)
                     print(i,xPos,yPos)
-        #设置帧率
-        cTime =time.time()
+        #set frame rate
+        cTime = time.time()
         fps=1/(cTime-pTime)
-        pTime =cTime
-        #向当前屏幕输出打印字符串
-        cv2.putText(img,f'FPS : {int(fps)}',(30,50),cv2.FONT_HERSHEY_PLAIN,1,(255,0,0),3)
+        pTime = cTime
+        #output the print string to the current screen
+        cv2.putText(img,f'FPS: {int(fps)}',(30,50),cv2.FONT_HERSHEY_PLAIN,1,(255,0,0),3)
 
 
-        #显示摄像头窗口
+        # show the camera window
         cv2.imshow('image', img)
         q = cv2.waitKey(100)
         if q == ord('q'):
             break
 
-#常规操作，循环结束以后释放cap
+#regular operation, release cap after the loop ends
 cap.release()
-
-
